18fdcc6a86e17eb8ba5d90e0d818a4cde0049d68
shinseiki86 <shinseiki86@gmail.com>
2016-11-16 07:17:02 -0500

    Corrección migración CreateTableTipounidad

--- database/migrations/2016_11_15_031643_create_table_tipounidad.php
+++ database/migrations/2016_11_15_031643_create_table_tipounidad.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableTipoUnidad extends Migration
+class CreateTableTipounidad extends Migration
 {
 	/**
 	 * Run the migrations.
