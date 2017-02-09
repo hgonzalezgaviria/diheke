6dbc8bdfd0422b15620451f67f7666dbe269504c
Shinseiki86 <shinseiki86@gmail.com>
2017-02-09 00:41:07 -0500

    tabs para salas por sede

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,47 +1,62 @@
 @extends('layout')
 
 @section('head')
-	{!! Html::style('assets/css/bootstrap.vertical-tabs.css') !!}
-	{!! Html::style('assets/css/hover-zoom.css') !!}
+  {!! Html::style('assets/css/bootstrap.vertical-tabs.css') !!}
+  {!! Html::style('assets/css/hover-zoom.css') !!}
 @endsection
 
 @section('scripts')
-	<script type="text/javascript">
-		$(document).ready(function () {
+  <script type="text/javascript">
+    $(document).ready(function () {
 
-			//var sedes = {!! $sedes !!};
-			var salas = {!! $salas !!};
+      //var sedes = {!! $sedes !!};
+      var salas = {!! $salas !!};
 
-			$('.tabs-left a').click(function (e) {
-			  e.preventDefault()
-			  var navTop = $('.nav-top a');
-			  navTop.attr('data-sede', $(this).data('sede'));
-			  //$(this).tab('show')
-			})
+      $('.tabs-left a').click(function (e) {
+        e.preventDefault()
+        var tabContent = $('.tab-content').find('#salas');
+        tabContent.empty();
 
-			$('#tab-salas').click(function (e) {
-			  e.preventDefault()
-			  var tabContent = $('.tab-content').find('#salas');
-			  tabContent.empty();
+        var navTop = $('.tabs-top a');
+        navTop.data('sede', $(this).data('sede'));
+        $('.tabs-top li').removeClass('hide');
+        $('.tabs-top a:first').trigger('click');
 
-			  for (var i = 0 ; i < salas.length; i++) {
-			  	if(salas[i].SEDE_ID == $(this).data('sede')){
-			   	  tabContent.append(salas[i].SALA_DESCRIPCION+'<br>');
-			    }
-			  }
+        //$(this).tab('show')
+      })
 
-			  
-			  //$(this);
-			  $(this).tab('show')
-			})
+      $('#tab-salas').click(function (e) {
+        e.preventDefault()
+        var tabContent = $('.tab-content').find('#salas');
+        tabContent.empty();
 
-			$('#tab-salas').on('shown.bs.tab', function (e) {
-			  console.log(e.target) // newly activated tab
-			  console.log(e.relatedTarget) // previous active tab
-			})
+        for (var i = 0 ; i < salas.length; i++) {
+          if(salas[i].SEDE_ID == $(this).data('sede')){
 
-		});
-	</script>
+            var html = '<div class="col-md-4 zoom-in-hover">'+
+                          '<div class="panel panel-default">'+
+                            '<div class="panel-heading">'+salas[i].SALA_DESCRIPCION+'<br></div>'+
+                              '<div class="panel-body">'+
+                              'Texto'+
+                            '</div>'+
+                          '</div>'+
+                        '</div>';
+            tabContent.append(html);
+          }
+        }
+
+        
+        //$(this);
+        $(this).tab('show')
+      })
+
+      $('#tab-salas').on('shown.bs.tab', function (e) {
+        //console.log(e.target) // newly activated tab
+        //console.log(e.relatedTarget) // previous active tab
+      })
+
+    });
+  </script>
 @parent
 @endsection
 
@@ -49,24 +64,24 @@
 <h1 class="page-header">Inicio</h1>
 
 <div class="col-xs-2"><h3>Sedes</h3>
-	<ul class="nav nav-tabs tabs-left">
-		@foreach($sedes as $sede)
-		<li><a href="#Sede{{$sede->SEDE_ID}}" data-toggle="tab" data-sede="{{$sede->SEDE_ID}}">{{$sede->SEDE_DESCRIPCION}}</a></li>
-		@endforeach
-	</ul>
+  <ul class="nav nav-tabs tabs-left">
+    @foreach($sedes as $sede)
+    <li><a href="#Sede{{$sede->SEDE_ID}}" data-toggle="tab" data-sede="{{$sede->SEDE_ID}}">{{$sede->SEDE_DESCRIPCION}}</a></li>
+    @endforeach
+  </ul>
 </div>
 
 <div class="col-xs-10">
-			<ul class="nav nav-tabs nav-top">
-				<li><a href="#salas" id="tab-salas" data-toggle="tab" data-sede="">Salas</a></li>
-				<li><a href="#equipos" id="tab-equipos" data-toggle="tab" data-sede="">Equipos</a></li>
-			</ul>
+      <ul class="nav nav-tabs tabs-top">
+        <li class="hide"><a href="#salas" id="tab-salas" data-toggle="tab" data-sede="">Salas</a></li>
+        <li class="hide"><a href="#equipos" id="tab-equipos" data-toggle="tab" data-sede="">Equipos</a></li>
+      </ul>
 </div>
 
 
 <div class="tab-content">
-	<div class="tab-pane fade" id="salas"></div>
-	<div class="tab-pane fade" id="equipos"></div>
-</div>	  
+  <div class="tab-pane fade" id="salas"></div>
+  <div class="tab-pane fade" id="equipos"></div>
+</div>    
 
 @endsection
